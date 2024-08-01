cf09fa2141640e242edc05950fd558fdffbf0179
Salman Ali <salmanafridi945@gmail.com>
2024-08-01 21:25:51 +0500

    added all the products by making a function homeProductCard in which i used foreach loop and destructure all the elements in the array of products from api and by suing importNode method i clone all the elements inside the template fragements.  some change in index file like ID and classes of the elements.

--- homeProductCards.js
+++ homeProductCards.js
@@ -1,12 +1,27 @@
-const productContainer = document.querySelector(".productContainer");
-const productTemplate = document.querySelector(".productTemplate");
+const productContainer = document.querySelector('#productContainer');
+const productTemplate = document.querySelector('#productTemplate');
 
 export const showProductContainer = (products) => {
   if (!products) {
     return "Empty";
   }
   products.forEach((curProd) => {
-    const { brand, category, description, id, image, name, price, stock } =
+    const { category, description,  image, name, price, stock } =
       curProd;
+
+
+      const productClone=document.importNode(productTemplate.content,true);
+
+      productClone.querySelector('.productName').textContent=name;
+      productClone.querySelector('.category').textContent=category;
+      productClone.querySelector('.productImage').src=image;
+      productClone.querySelector('.productImage').alt=name;
+      productClone.querySelector('.productDescription').textContent=description;
+      productClone.querySelector('.productPrice').textContent=`$ ${price}`;
+      productClone.querySelector('.productStock').textContent=stock;
+      productClone.querySelector('.productActualPrice').textContent=`$ ${price*3}`;
+
+      productContainer.append(productClone);
   });
 };
+  
