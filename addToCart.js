9cbe7ea2dda159e35bd84499fad811aa4966e162
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-08-05 04:39:33 +0500

    add set and get method of localstorage

--- addToCart.js
+++ addToCart.js
@@ -1,9 +1,20 @@
+import { getCartProductFromLS } from "./getCartProductFromLS.JS";
+
 export const addToCart=(event,id,stock)=>{
 
+    let arrLocalStorageProduct=getCartProductFromLS();
     const currentElement=document.querySelector(`#card${id}`);
 
     let quantity=currentElement.querySelector('.productQuantity').innerText;
     let price=currentElement.querySelector('.productPrice').innerText;
 
-    console.log(price,quantity);
+    // console.log(price,quantity);
+
+    price=price.replace("$","");
+    price=Number(price*quantity);
+    quantity=Number(quantity);
+    // console.log(price,quantity);
+
+    arrLocalStorageProduct.push({id,quantity,price});
+    localStorage.setItem('cartProductLS',JSON.stringify(arrLocalStorageProduct));
 };
