e0bbce2eb803c9253783bf3ccecf642688a86736
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-08-06 03:25:07 +0500

    added the logical part of the price and quantity of the product if any one click on add to cart button and produc is same so it should only increase the price and quantity of that product.

--- addToCart.js
+++ addToCart.js
@@ -15,6 +15,20 @@ export const addToCart = (event, id, stock) => {
   price = price.replace("$", "");
 
   let existingProd = arrLocalStorageProduct.find((curProd) => curProd.id == id);
+
+if (existingProd&&quantity>1) {
+  quantity=Number(existingProd.quantity)+Number(quantity);
+  price = Number(price * quantity);
+
+  let updatedCart={id,quantity,price};
+ updatedCart= arrLocalStorageProduct.map((curProd)=>{
+   return curProd.id==id ? updatedCart:curProd;
+  });
+  console.log(updatedCart);
+  
+  localStorage.setItem("cartProductLS", JSON.stringify(updatedCart));
+}
+
   if (existingProd) {
     return false;
   }
