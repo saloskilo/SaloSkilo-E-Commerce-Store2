0a4b3750c04f42d1e63b67527ace3bbc6e2bfe49
Salman Ali <salmanafridi945@gmail.com>
2024-08-06 22:35:36 +0500

    adding elements in the addtocart page using foreach and clone all the product from local storage.

--- showAddToCards.js
+++ showAddToCards.js
@@ -9,3 +9,28 @@ let filterProducts = products.filter((curProd) => {
   });
 });
 console.log(filterProducts);
+
+
+const cartElement = document.querySelector('#productCartContainer');
+const templateContainer = document.querySelector('#productContainer');
+
+
+const showCartProducts = () => {
+  filterProducts.forEach((curProd) => {
+    const { category, id, image, name, stock, price } = curProd;
+
+    let productClone = document.importNode(templateContainer.Content, true);
+
+    productClone.querySelector('#cardvalue').setAttribute('id',`card${id}`);
+
+    productClone.querySelector('.productName').textContent=name;
+    productClone.querySelector('.category').textContent=category;
+    productClone.querySelector('.productImage').src=image;
+    cartElement.appendChild(productClone);
+
+  });
+}
+
+
+
+showCartProducts();
