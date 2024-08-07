bd499a680d05873df47b49769d10d3da90aae1b1
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-08-07 05:36:00 +0500

    removed some bugs from code and make a function to fetch data for add to cart page from localstorage for the quantity and price.

---
+++ fetchQuantityFromCartLS.js
@@ -0,0 +1,14 @@
+import { getCartProductFromLS } from "./getCartProductFromLS.JS";
+
+export const fetchQuantityFromCartLS = (id, price) => {
+  let cartProducts = getCartProductFromLS();
+
+  let existingProduct = cartProducts.find((curProd) => curProd.id === id);
+  let quantity = 1;
+
+  if (existingProduct) {
+    quantity = existingProduct.quantity;
+    price = existingProduct.price;
+  }
+  return { quantity, price };
+};
--- showAddToCards.js
+++ showAddToCards.js
@@ -1,31 +1,37 @@
 import products from "./api/products.json";
+import { fetchQuantityFromCartLS } from "./fetchQuantityFromCartLS";
 import { getCartProductFromLS } from "./getCartProductFromLS.JS";
 
 let cartProducts = getCartProductFromLS();
 
 let filterProducts = products.filter((curProd) => {
-  return cartProducts.some((curElem) => {
-    curElem.id === curProd.id;
-  });
+  return cartProducts.some((curElem) =>  curElem.id === curProd.id );
 });
 console.log(filterProducts);
 
 
 const cartElement = document.querySelector('#productCartContainer');
-const templateContainer = document.querySelector('#productContainer');
+const templateContainer = document.querySelector('#productCartTemplate');
 
 
 const showCartProducts = () => {
   filterProducts.forEach((curProd) => {
     const { category, id, image, name, stock, price } = curProd;
 
-    let productClone = document.importNode(templateContainer.Content, true);
+    let productClone = document.importNode(templateContainer.content, true);
 
-    productClone.querySelector('#cardvalue').setAttribute('id',`card${id}`);
+    const LSActualData=fetchQuantityFromCartLS(id,price);
+console.log(LSActualData);
 
+    productClone.querySelector('#cardValue').setAttribute('id',`card${id}`);
     productClone.querySelector('.productName').textContent=name;
     productClone.querySelector('.category').textContent=category;
     productClone.querySelector('.productImage').src=image;
+
+    productClone.querySelector('.productQuantity').textContent=LSActualData.quantity;
+    productClone.querySelector('.productPrice').textContent=LSActualData.price;
+
+
     cartElement.appendChild(productClone);
 
   });
